88ced7e1cca5c586ed3be9faeca67a71241ebe76
SAI1029 <89194944+SAI1029@users.noreply.github.com>
2022-06-09 12:13:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+music_1 = "";
+music_2 = "";
+
+function preload(){
+    music_1 = loadSound("butter_bts.mp3");
+    music_2 = loadSound("music.mp3");
+}
+
+function setup(){
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw(){
+    image(video, 0, 0, 600, 500);
+}
+
+function play(){
+    music_1.play();
+}
